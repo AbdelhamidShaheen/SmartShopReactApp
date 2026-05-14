46b4add81a2586cbffcc6fd901f192f2a2765b1d
abdelhamid_shaheen <abdelhamid_shaheen@yahoo.com>
2026-05-14 20:46:24 +0300

    install redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routing/index.jsx";
+import store from "./Store/index.js";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 );
